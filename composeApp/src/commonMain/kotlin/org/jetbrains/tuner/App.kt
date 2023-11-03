a729fda3b36b7f42568f6f7e271f0e8d2ab0ae36
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2023-11-03 12:28:51 +0100

    Add labels

--- composeApp/src/commonMain/kotlin/org/jetbrains/tuner/App.kt
+++ composeApp/src/commonMain/kotlin/org/jetbrains/tuner/App.kt
@@ -1,17 +1,17 @@
 package org.jetbrains.tuner
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.text.*
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
@@ -33,11 +33,20 @@ internal fun App() = AppTheme {
         detector.startDetector()
     }
 
-    Column {
-        Text("Freq=$freq")
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            "${
+                freq.toString().let {
+                    val parts = it.split('.')
+                    parts.first() + '.' + parts.getOrElse(1, {"0"}).take(2)
+                }
+            } Hz"
+        )
 
         Freqometr(
-            modifier = Modifier.fillMaxSize().padding(16.dp),
+            modifier = Modifier.fillMaxSize().aspectRatio(2f).padding(16.dp),
             angle = 80f,
             min = 200f,
             max = 500f,
@@ -57,11 +66,15 @@ private fun Freqometr(
     tickNumber: Int,
     tickColor: Color = MaterialTheme.colorScheme.onSurface
 ) {
+    val textMeasurer = rememberTextMeasurer()
     Canvas(modifier) {
         scale(
             angle = angle,
             tickNumber = tickNumber,
             tickColor = tickColor,
+            textMeasurer = textMeasurer,
+            min = min,
+            max = max,
             getTickHeight = { index ->
                 when {
                     index % 10 == 0 -> 24.dp
@@ -83,6 +96,10 @@ private fun DrawScope.scale(
     tickNumber: Int,
     tickColor: Color = Color.Black,
     tickWidth: Dp = 2.dp,
+    textMeasurer: TextMeasurer,
+    textStyle: TextStyle = TextStyle(),
+    min: Float,
+    max: Float,
     getTickHeight: (Int) -> Dp = { 16.dp }
 ) {
     val radius = size.height
@@ -110,6 +127,35 @@ private fun DrawScope.scale(
             strokeWidth = tickWidth.toPx(),
             cap = StrokeCap.Round
         )
+
+        val withLabel = when (tick) {
+            0 -> max
+            tickNumber - 1 -> min
+            else -> null
+        }
+
+        if (withLabel != null) {
+            val label = "$withLabel Hz"
+            val labelTextLayout = textMeasurer.measure(
+                text = label,
+                style = textStyle
+            )
+            val labelOffset = Offset(
+                x = scaleCenterX + (radius - tickHeight - 16.dp.toPx()) * cos((tickAngle + rotation) * (PI / 180)).toFloat(),
+                y = scaleCenterY - (radius - tickHeight - 16.dp.toPx()) * sin((tickAngle + rotation) * (PI / 180)).toFloat()
+            )
+            val labelTopLeft = Offset(
+                labelOffset.x - (labelTextLayout.size.width / 2f),
+                labelOffset.y - (labelTextLayout.size.height / 2f)
+            )
+
+            drawText(
+                textMeasurer = textMeasurer,
+                text = label,
+                topLeft = labelTopLeft,
+                style = textStyle
+            )
+        }
     }
 }
 
